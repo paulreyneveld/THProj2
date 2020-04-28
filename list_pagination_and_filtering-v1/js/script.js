f70d6977f9e3f27338183337b40d581a70d5e950
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-04-28 15:42:29 -0700

    Search function implemented

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -3,13 +3,15 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
+// Constants collect the two variables required to meet the baseline requirements of this project.
 const list = document.querySelectorAll('li');
-const itemsPerPage = 10;
+const itemsPerPage = 10; // This number can be altered to set pagination length.
 
-
-let newDiv = document.createElement("div");
-let newInput = document.createElement("input");
-let newButton = document.createElement("button");
+// The following outputs HTML to the page to allow for the usage of a search bar.
+// I've left it in because I succeeded at part of the extra credit. . . .
+const newDiv = document.createElement("div");
+const newInput = document.createElement("input");
+const newButton = document.createElement("button");
 newDiv.className = "student-search";
 document.querySelector(".page-header").appendChild(newDiv);
 document.querySelector(".student-search").appendChild(newInput);
@@ -18,23 +20,11 @@ newButton.textContent = "Search";
 newInput.placeholder = "Search for students...";
 newInput.type = "text";
 newInput.id = "search-input";
-let searchInput = document.querySelector("#search-input");
-console.log(list[0].textContent);
-function searchFun(searchInput, list) {
-	console.log(searchInput);
-	console.log(searchInput.value);
-	for (let i = 0; i < list.length; i += 1) {
-		list[i].style.display = 'none';
-		if (searchInput.value.length != 0 && list[i].textContent.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1) {
-			list[i].style.display = 'list-item';
-			console.log("I've found it");
-		}
-	}
-	
-}
 
+// showPage requires a list and a page number. It's called initially
+// to show the first 10 elements of the list and then called again
+// if the search bar has been used but then left blank again.
 function showPage(list, page) {
-
 	const startInd = (page * itemsPerPage) - itemsPerPage;
 	const endInd = page * itemsPerPage;
 	for (let i = 0; i < list.length; i += 1) {
@@ -47,6 +37,10 @@ function showPage(list, page) {
 	}
 }
 
+// appendPageLinks takes a list and determines how many links will
+// be required to navigate the data (dependent on itemsPerPage, see above).
+// It then dynamically creates a navigation system for the links.
+// It also uses an event listener to highlight the active link. 
 function appendPageLinks(list) {
 	numOfLinks = Math.ceil(list.length / itemsPerPage);
 
@@ -75,16 +69,48 @@ function appendPageLinks(list) {
 	
 }
 
+// What follows is my attempt at the extra credit. 
+// I wasn't able to get the pagination or 'item not found'
+// aspect of the project working, and while I love to exceed
+// expecations, I've been banging my head against a wall and 
+// need to move on. 
+
+// Obtains search criteria from the DOM 
+let searchInput = document.querySelector("#search-input ");
+
+// Searchfun takes two inputs, searchInput and list.
+// It dynamically compares the name contained within each
+// list item to the searchInput string and outputs 
+// the list items that have matching values. 
+
+function searchFun(searchInput, list) {
+	for (let i = 0; i < list.length; i += 1) {
+		list[i].style.display = 'none';
+		let nameSearched = list[i].querySelector(".student-details h3").innerText;
+		if (searchInput.value.length != 0 && nameSearched.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1) {
+			list[i].style.display = 'list-item';
+		}
+		else if (searchInput.value.length == 0) {
+			showPage(list, 1);
+		}
+	}
+	
+}
+
+// Calls showPage function.
 showPage(list, 1);
+
+// Call appendPageLinks function.
 appendPageLinks(list);
+
+// Handles search submission on button click.
 newButton.addEventListener('click', (e) => {
 	event.preventDefault();
 	searchFun(searchInput, list);
-	console.log('Submit function is working!');
 });
 
+// Handles search as keys are entered into the input box. 
 newInput.addEventListener('keyup', (e) => {
 	event.preventDefault();
 	searchFun(searchInput, list);
-	console.log("Keylog is functioning");
 });
